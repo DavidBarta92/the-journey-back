abd861942bf5092107e3fad1850f0c1a01711ec3
Barta Dávid <barta.david@soulware.hu>
2024-01-19 20:12:45 +0100

    blur

--- src/views/filter.js
+++ src/views/filter.js
@@ -1,4 +1,5 @@
 import gameCanvas from '../models/gameCanvas'
+import stackblur from 'stackblur-canvas';
 
 var Filter = (function () {
   var r = Math.random
@@ -152,6 +153,8 @@ var Filter = (function () {
           : white.a
     }
   }
+  const StackBlur = require('stackblur-canvas');
+
 
   /**
    * Blur image data.
@@ -161,108 +164,16 @@ var Filter = (function () {
    * @return {ImageData} The processed image data with blur.
    */
   function doBlur (imageData, radius, quality) {
-    var pixels = imageData.data
-    var width = imageData.width
-    var height = imageData.height
+    
+    const width = imageData.width;
+    const height = imageData.height;
 
-    var rsum, gsum, bsum, asum, x, y, i, p, p1, p2, yp, yi, yw
-    var wm = width - 1
-    var hm = height - 1
-    var rad1x = radius + 1
-    var divx = radius + rad1x
-    var rad1y = radius + 1
-    var divy = radius + rad1y
-    var div2 = 1 / (divx * divy)
+    const blurredImageData = new ImageData(width, height);
+    blurredImageData.data.set(imageData.data);
 
-    var r = []
-    var g = []
-    var b = []
-    var a = []
+    
 
-    var vmin = []
-    var vmax = []
-
-    while (quality-- > 0) {
-      yw = yi = 0
-
-      for (y = 0; y < height; y++) {
-        rsum = pixels[yw] * rad1x
-        gsum = pixels[yw + 1] * rad1x
-        bsum = pixels[yw + 2] * rad1x
-        asum = pixels[yw + 3] * rad1x
-
-        for (i = 1; i <= radius; i++) {
-          p = yw + ((i > wm ? wm : i) << 2)
-          rsum += pixels[p++]
-          gsum += pixels[p++]
-          bsum += pixels[p++]
-          asum += pixels[p]
-        }
-
-        for (x = 0; x < width; x++) {
-          r[yi] = rsum
-          g[yi] = gsum
-          b[yi] = bsum
-          a[yi] = asum
-
-          if (y === 0) {
-            vmin[x] = Math.min(x + rad1x, wm) << 2
-            vmax[x] = Math.max(x - radius, 0) << 2
-          }
-
-          p1 = yw + vmin[x]
-          p2 = yw + vmax[x]
-
-          rsum += pixels[p1++] - pixels[p2++]
-          gsum += pixels[p1++] - pixels[p2++]
-          bsum += pixels[p1++] - pixels[p2++]
-          asum += pixels[p1] - pixels[p2]
-
-          yi++
-        }
-        yw += width << 2
-      }
-
-      for (x = 0; x < width; x++) {
-        yp = x
-        rsum = r[yp] * rad1y
-        gsum = g[yp] * rad1y
-        bsum = b[yp] * rad1y
-        asum = a[yp] * rad1y
-
-        for (i = 1; i <= radius; i++) {
-          yp += i > hm ? 0 : width
-          rsum += r[yp]
-          gsum += g[yp]
-          bsum += b[yp]
-          asum += a[yp]
-        }
-
-        yi = x << 2
-        for (y = 0; y < height; y++) {
-          pixels[yi] = (rsum * div2 + 0.5) | 0
-          pixels[yi + 1] = (gsum * div2 + 0.5) | 0
-          pixels[yi + 2] = (bsum * div2 + 0.5) | 0
-          pixels[yi + 3] = (asum * div2 + 0.5) | 0
-
-          if (x === 0) {
-            vmin[y] = Math.min(y + rad1y, hm) * width
-            vmax[y] = Math.max(y - radius, 0) * width
-          }
-
-          p1 = x + vmin[y]
-          p2 = x + vmax[y]
-
-          rsum += r[p1] - r[p2]
-          gsum += g[p1] - g[p2]
-          bsum += b[p1] - b[p2]
-          asum += a[p1] - a[p2]
-
-          yi += width << 2
-        }
-      }
-    }
-    return imageData
+    return StackBlur.imageDataRGBA(imageData,0,0, width, height, 5);
   }
   /**
    * Generate a random number within a specified range.
@@ -357,15 +268,13 @@ var Filter = (function () {
      * @return {ImageData|null} The processed image data, or null if an error occurs.
      */
     blur: function (image, radius, quality) {
+      console.log(image);
       if (radius < 0) {
         radius = 0
       }
-      try {
         return doBlur(image, radius, quality)
-      } catch (error) {
-        console.error(error)
-        return null
-      }
+
+
     },
     /**
      * Apply raindrops effect to an image.
