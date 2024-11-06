e1c81a1535cec9c83b6a242783b998c70f45ddf0
Barta Dávid <barta.david@soulware.hu>
2024-11-06 21:36:11 +0100

    random sprites

--- src/models/sprites.js
+++ src/models/sprites.js
@@ -1,21 +1,45 @@
 
-export const tree = {
+export const rock_1 = {
     x: 311,
     y: 42,
     w: 33,
     h: 18
 };
-export const rock = {
+export const rock_2 = {
     x: 343,
     y: 9,
     w: 44,
     h: 25
 };
-export const cross = {
-    x: 0,
-    y: 0,
-    w: 0,
-    h: 0
+export const rock_3 = {
+    x: 400,
+    y: 51,
+    w: 60,
+    h: 38
+};
+export const rock_4 = {
+    x: 359,
+    y: 55,
+    w: 25,
+    h: 22
+};
+export const rock_5 = {
+    x: 283,
+    y: 74,
+    w: 40,
+    h: 24
+};
+export const rock_6 = {
+    x: 383,
+    y: 39,
+    w: 23,
+    h: 13
+};
+export const rock_7 = {
+    x: 325,
+    y: 35,
+    w: 22,
+    h: 16
 };
 
 export const background = {
